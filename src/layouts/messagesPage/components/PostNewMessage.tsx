480cbea4e9814e66f47075fbfcb52e2e01107f27
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-22 17:18:48 +0100

    Adding the post question functionality

--- src/layouts/messagesPage/components/PostNewMessage.tsx
+++ src/layouts/messagesPage/components/PostNewMessage.tsx
@@ -1,5 +1,6 @@
 import {useOktaAuth} from "@okta/okta-react";
 import {useState} from "react";
+import MessageModel from "../../../models/MessageModel";
 
 export const PostNewMessage = () => {
 
@@ -9,13 +10,32 @@ export const PostNewMessage = () => {
     const [displayWarning, setDisplayWarning] = useState(false);
     const [displaySuccess, setDisplaySuccess] = useState(false);
 
+    const submitNewQuestion = async () => {
+        const url = `http://localhost:8080/api/messages/secure/add/message`;
+        if(authState?.isAuthenticated && title !== '' && question !== ''){
+            const messageRequestModel: MessageModel = new MessageModel(title, question);
+            const requestOptions = {
+                method: 'POST',
+                headers : {
+                    Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(messageRequestModel)
+            };
+            const submitNewQuestionResponse = await fetch(url, requestOptions);
+            if(!submitNewQuestionResponse.ok) throw new Error('Something went wrong!');
+            setTitle('');
+            setQuestion('');
+            setDisplayWarning(false);
+            setDisplaySuccess(true);
+        } else {
+            setDisplayWarning(true);
+            setDisplaySuccess(false);
+        }
+    };
+
     return (
         <div className='card mt-3'>
-            {displaySuccess &&
-                <div className='alert alert-success' role='alert'>
-                    Question added successfully
-                </div>
-            }
             <div className='card-header'>
                 Ask question to NizaRead Admin
             </div>
@@ -24,6 +44,11 @@ export const PostNewMessage = () => {
                     {displayWarning &&
                         <div className='alert alert-danger' role='alert'>
                             All fields must be filled out
+                        </div>
+                    }
+                    {displaySuccess &&
+                        <div className='alert alert-success' role='alert'>
+                            Question added successfully
                         </div>
                     }
                     <div className='mb-3'>
@@ -42,7 +67,7 @@ export const PostNewMessage = () => {
                         </textarea>
                     </div>
                     <div>
-                        <button type='button' className='btn btn-primary mt-3'>
+                        <button type='button' className='btn btn-primary mt-3' onClick={submitNewQuestion}>
                             Submit Question
                         </button>
                     </div>
