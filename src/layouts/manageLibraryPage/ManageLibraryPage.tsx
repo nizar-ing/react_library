36e6cf9c07f0ffb50f0cfdddf5df6dc0d3b76b3b
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-23 15:56:03 +0100

    Creating our Admin Message Component

--- src/layouts/manageLibraryPage/ManageLibraryPage.tsx
+++ src/layouts/manageLibraryPage/ManageLibraryPage.tsx
@@ -1,6 +1,7 @@
 import {useOktaAuth} from "@okta/okta-react";
 import {useState} from "react";
 import {Redirect} from "react-router-dom";
+import {AdminMessages} from "./components/AdminMessages";
 
 export const ManageLibraryPage = () => {
     const { authState } = useOktaAuth();
@@ -61,7 +62,7 @@ export const ManageLibraryPage = () => {
                         {changeQuantityOfBooksClick ? <>Change Quantity</> : <></>}
                     </div>
                     <div className='tab-pane fade' id='nav-messages' role='tabpanel' aria-labelledby='nav-messages-tab'>
-                        {messagesClick ? <>Admin Messages</> : <></>}
+                        {messagesClick ? <AdminMessages /> : <></>}
                     </div>
                 </div>
             </div>
