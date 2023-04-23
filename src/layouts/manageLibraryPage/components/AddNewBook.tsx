36f49b548daeb5d21c895379dc8a84b149fda4e8
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-23 22:11:02 +0100

    Submitting our new book

--- src/layouts/manageLibraryPage/components/AddNewBook.tsx
+++ src/layouts/manageLibraryPage/components/AddNewBook.tsx
@@ -1,5 +1,6 @@
 import {useOktaAuth} from "@okta/okta-react";
 import {useState} from "react";
+import AddBookRequest from "../../../models/AddBookRequest";
 
 export const AddNewBook = () => {
     const { authState } = useOktaAuth();
@@ -35,6 +36,35 @@ export const AddNewBook = () => {
         reader.onerror = (error) => {
           console.error('Error', error);
         };
+    };
+
+    const submitNewBook = async () => {
+        const url = `http://localhost:8080/api/admin/secure/add/book`;
+        if(authState?.isAuthenticated && title !== '' && author !== '' && category !== 'Category' && description !== '' && copies >= 0){
+            const book: AddBookRequest = new AddBookRequest(title, author, description, copies, category);
+            book.img = selectedImage;
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(book)
+            };
+            const submitNewBookResponse = await fetch(url, requestOptions);
+            if(!submitNewBookResponse.ok) throw new Error('Something went wrong');
+            setTitle('');
+            setAuthor('');
+            setDescription('');
+            setCopies(0);
+            setCategory('Category');
+            setSelectedImage(null);
+            setDisplayWarning(false);
+            setDisplaySuccess(true);
+        } else {
+            setDisplayWarning(true);
+            setDisplaySuccess(false);
+        }
     };
 
     return(
@@ -92,7 +122,7 @@ export const AddNewBook = () => {
                         </div>
                         <input type='file' onChange={(e) => base64ConversionForImages(e)} />
                         <div>
-                            <button type='button' className='btn btn-primary mt-3'>
+                            <button type='button' className='btn btn-primary mt-3' onClick={submitNewBook}>
                                 Add Book
                             </button>
                         </div>
