2e0f27fbcb56bc39f524fc6123b5574feddcb487
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-24 23:32:21 +0100

    Updating our Okta Redirect Uri in our React app

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig ={
     clientId: '0oa8wcauk43hTL56I5d7',
     issuer: 'https://dev-38335434.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true
